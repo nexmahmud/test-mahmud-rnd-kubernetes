70cae7219c9d134dd2d4e1b01872f42e2162e6df
nexmahmud <126381713+nexmahmud@users.noreply.github.com>
2023-06-21 15:08:33 +0600

    Add files via upload

---
+++ src/main/java/com/example/testmahmudrnd/TestMahmudRndApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmahmudrnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMahmudRndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMahmudRndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testmahmudrnd/TestMahmudRndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testmahmudrnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMahmudRndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
